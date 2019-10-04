21c63d4190a8cc7cd0b6f819b55f1029e48cff84
ConnectedSystems <takuyai@gmail.com>
2019-10-04 23:14:25 +1000

    More explicit maintenance attributes

--- src/agtor/FieldComponent.py
+++ src/agtor/FieldComponent.py
@@ -1,26 +1,32 @@
 from typing import Tuple
 from dataclasses import dataclass
 
+from .Component import Component
+
 @dataclass
-class Infrastructure:
+class Infrastructure(Component):
     """Represents generic farm infrastructure."""
     name: str
     capital_cost_per_ha: float
     # num years maintenance occurs, and 
     # assumed proportion of capital cost
-    minor_maintenance_rate: Tuple[float]  
-    major_maintenance_rate: Tuple[float]
+    minor_maintenance_rate: float
+    major_maintenance_rate: float
+    minor_maintenance_schedule: float
+    major_maintenance_schedule: float
     implemented: bool
 
     def __post_init__(self):
         minor_mr = self.minor_maintenance_rate
         major_mr = self.major_maintenance_rate
+
         self.maintenance_year = {
-            'minor': minor_mr[0],
-            'major': major_mr[0]
+            'minor': self.minor_maintenance_schedule,
+            'major': self.major_maintenance_schedule
         }
-        self.minor_maintenance_cost = self.capital_cost_per_ha * minor_mr[1]
-        self.major_maintenance_cost = self.capital_cost_per_ha * major_mr[1]
+
+        self.minor_maintenance_cost = self.capital_cost_per_ha * minor_mr
+        self.major_maintenance_cost = self.capital_cost_per_ha * major_mr
     # End __post_init__()
 
     def maintenance_cost(self, year_step: int) -> float:
