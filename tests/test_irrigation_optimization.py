c993b8ca2fadf10b1de3059cba3e611651577636
ConnectedSystems <takuyai@gmail.com>
2019-10-05 10:36:23 +1000

    Adjusted manual component creation

--- tests/test_irrigation_optimization.py
+++ tests/test_irrigation_optimization.py
@@ -1,13 +1,53 @@
 from agtor import (Irrigation, Pump, Crop, CropField, FarmZone, Manager, WaterSource)
 
+import pandas as pd
+
 def setup_zone():
     irrig = Irrigation('Gravity', 2000.0, 1, 5, 0.05, 0.2, True, 0.6, 12, 10)
 
     # None values represent growth_stages data which I haven't converted yet.
     crop_rotation = [
-        Crop('Wheat', None, 3.5, 180.0, 100.0, 0.5),
-        Crop('Barley', None, 3.5, 180.0, 100.0, 0.5),
-        Crop('Canola', None, 3.5, 180.0, 100.0, 0.5)
+        Crop('Wheat', 'irrigated_cereal', '05-15', 
+             yield_per_ha=3.5, 
+             price_per_yield=180.0, 
+             variable_cost_per_ha=100.0, 
+             root_depth_m=1.0,
+             effective_root_zone=0.55,
+             water_use_ML_per_ha=3.0,
+             growth_stages={
+                 'initial': {
+                     'stage_length': 40,
+                     'depletion_fraction': 0.55
+                 }
+             }),
+        Crop('Barley', 'irrigated_cereal', '05-15',
+             yield_per_ha=3.5, 
+             price_per_yield=180.0, 
+             variable_cost_per_ha=100.0, 
+             root_depth_m=1.0,
+             effective_root_zone=0.55,
+             water_use_ML_per_ha=3.0,
+             growth_stages={
+                 'initial': {
+                     'stage_length': 40,
+                     'crop_coefficient': 0.15,
+                     'depletion_fraction': 0.55
+                 }
+             }),
+        Crop('Canola', 'irrigated_cereal', '05-15',
+             yield_per_ha=3.5, 
+             price_per_yield=180.0, 
+             variable_cost_per_ha=100.0, 
+             root_depth_m=1.0,
+             effective_root_zone=0.55,
+             water_use_ML_per_ha=3.0,
+             growth_stages={
+                 'initial': {
+                     'stage_length': 40,
+                     'crop_coefficient': 0.15,
+                     'depletion_fraction': 0.55
+                 }
+             })
     ]
 
     shallowpump = Pump('surface_water', 2000.0, 1, 5, 0.05, 0.2, True, 0.7, 0.28, 0.75)
@@ -26,8 +66,8 @@ def setup_zone():
                             pump=deeppump
                         )
 
-    field1 = CropField('1', 100.0, irrig, crop_rotation, 100.0, 50.0, 20.0)
-    field2 = CropField('2', 90.0, irrig, crop_rotation, 100.0, 50.0, 30.0)
+    field1 = CropField('field1', 100.0, irrig, crop_rotation, 100.0, 50.0, 20.0)
+    field2 = CropField('field2', 90.0, irrig, crop_rotation, 100.0, 50.0, 30.0)
 
     z1 = FarmZone('Zone_1', climate=None, 
                   fields=[field1, field2],
@@ -40,16 +80,21 @@ def test_naive_management():
     z1, channel_water, deeplead = setup_zone()
 
     Farmer = Manager()
+    dt = pd.to_datetime('1981-01-01')
     opt_results = Farmer.optimize_irrigated_area(z1, 1)
-
-    print(opt_results)
 # End test_naive_management()
 
 def test_zone_management():
     z1, channel_water, deeplead = setup_zone()
 
     Farmer = Manager()
-    opt_results = Farmer.optimize_irrigation(z1, 1)
+    opt_results = Farmer.optimize_irrigated_area(z1, 1)
+
+    for f in z1.fields:
+        f.irrigated_area = Farmer.get_optimum_irrigated_area(f, opt_results)
+
+    dt = pd.to_datetime('1981-01-01')
+    opt_results = Farmer.optimize_irrigation(z1, dt, 1)
 
     expected = [0.0, 100.0, 0.0, 90.0]
     opt = list(opt_results.values())
@@ -60,10 +105,10 @@ def test_zone_management():
         Raw: {}
         """.format(opt, expected, opt_results.values())
 
-    channel_water.head = 25.0
+    channel_water.head = 1000.0
     deeplead.head = 0.0
 
-    opt_results = Farmer.optimize_irrigation(z1, 1)
+    opt_results = Farmer.optimize_irrigation(z1, dt, 1)
 
     expected = [100.0, 0.0, 90.0, 0.0]
     opt = list(opt_results.values())
