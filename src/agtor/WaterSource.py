12b12dc60465778ab683d3700d901c601bd3d2a2
ConnectedSystems <takuyai@gmail.com>
2019-10-14 11:20:45 +1100

    Split out usage cost calculation

--- src/agtor/WaterSource.py
+++ src/agtor/WaterSource.py
@@ -23,8 +23,11 @@ class WaterSource(Component):
         return self.pump.pumping_costs_per_ML(flow_rate_Lps, self.head)
     # End pump_cost_per_ML()
 
+    def usage_costs(self, water_used_ML):
+        return self.cost_per_ML * water_used_ML
+
     def total_costs(self, area, water_used_ML):
-        usage_fee = self.cost_per_ML * water_used_ML
+        usage_fee = self.usage_costs(water_used_ML)
         area_fee = self.cost_per_ha * area
         return self.yearly_costs + usage_fee + area_fee
     # End total_costs()
