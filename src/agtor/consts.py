16926c4545d24d5973d5c6c7c2b652d4eb654d93
ConnectedSystems <takuyai@gmail.com>
2019-11-16 19:33:59 +1100

    Useful constants

--- src/agtor/consts.py
+++ src/agtor/consts.py
@@ -1,1 +1,16 @@
+# volume
 ML_to_mm = 100.0
+mm_to_ML = 100.0
+
+# distance
+km_to_ha = 100.0
+ha_to_km = 100.0
+
+# time
+SEC_IN_DAY = 86400.0
+
+# amount
+MILLION = 1e6
+ML = 1e6  # Litres in a megaliter
+
+
